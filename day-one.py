5f6e9c5e3ed00abd9de6c5d97ef9dbe998f0494e
Jens <jenslorden@gmail.com>
2022-12-01 18:21:43 +0100

    Day one part two solution

--- day-one.py
+++ day-one.py
@@ -10,27 +10,48 @@ def getData(path):
                 calories.append([])
                 continue
 
-            val = int(line[:-1])
+            val = int(line[:-1]) if '\n' in line else int(line)
             calories[-1].append(val)
-            continue
+
     return calories
 
 def getCaloriesSum(calories):
     calories_sum = [sum(row) for row in calories]
     return calories_sum
 
-def main():
-    """ Problem one test """
-    calories = getData('data/day-1-test.txt')
+def partOne(path):
+    calories = getData(path)
     cals_sum = getCaloriesSum(calories)
     max_cal = max(cals_sum)
-    assert(max_cal == 24000)
+    return max_cal
 
-    """ Problem one """
-    calories = getData('data/day-1.txt')
+def partTwo(path):
+    calories = getData(path)
     cals_sum = getCaloriesSum(calories)
-    max_cal = max(cals_sum)
-    print('Max cals: {}'.format(max_cal))
+    top_n = 3
+    total_max_cal = 0
+    for i in range(top_n):
+        max_cal = max(cals_sum)
+        total_max_cal += max_cal
+        cals_sum.remove(max_cal)
+    return total_max_cal
+
+def main():
+    """ Part one test """
+    res = partOne('data/day-1-test.txt')
+    assert(res == 24000)
+
+    """ Part one """
+    res = partOne('data/day-1.txt')
+    print('Part 1 solution: {}'.format(res))
+
+    """ Part two test """
+    res = partTwo('data/day-1-test.txt')
+    assert(res == 45000)
+
+    """ Part two """
+    res = partTwo('data/day-1.txt')
+    print('Part 2 solution: {}'.format(res))
 
     return 0
 

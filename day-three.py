cd11e25afe796e1114ec3aa09e58fb7659bb2d3b
celsuss <jens.lorden@imagimob.com>
2022-12-02 12:42:14 +0100

    Day three part two solved

--- day-three.py
+++ day-three.py
@@ -32,16 +32,41 @@ def getPartOneSolution(backpacks):
 
     return priority_sum
 
+def getSharedBadge(packs):
+    pack1_dict = packs[0]
+    pack2_dict = packs[1]
+    for item in packs[2]:
+        if item in pack1_dict and item in pack2_dict:
+            return item
+
+    return None
+
+def getPartTwoSolution(backpacks):
+    backpacks = backpacks.reshape(-1, 3)
+    priority_sum = 0
+    for packs in backpacks:
+        badge = getSharedBadge(packs)
+        priority_sum += getItemPriority(badge)
+        continue
+
+    return priority_sum
+
 def main():
     """ Tests """
     backpacks = np.genfromtxt('data/day-3-test.txt', dtype=str)
     res = getPartOneSolution(backpacks)
     assert(res == 157)
+    res = getPartTwoSolution(backpacks)
+    assert(res == 70)
 
     """ Solutions """
     backpacks = np.genfromtxt('data/day-3.txt', dtype=str)
     res = getPartOneSolution(backpacks)
+    assert(res == 8018)
     print('Part one solution: {}'.format(res))
+    res = getPartTwoSolution(backpacks)
+    assert(res == 2518)
+    print('Part two solution: {}'.format(res))
 
     return 0
 

f9a8cf5767b1c38264de990eb7ed03b1b0a2af69
Jens <jenslorden@gmail.com>
2022-12-06 21:24:30 +0100

    Day five part two solved

--- day-five.py
+++ day-five.py
@@ -1,3 +1,4 @@
+import numpy as np
 
 def getData(path):
     def getNCrates(lines):
@@ -41,16 +42,6 @@ def getData(path):
 
     return crates, moves
 
-def getDayOneSolution(crates, moves):
-    reverseCrates(crates)
-    for n_crates, start_crate, target_crate in moves:
-        for i in range(n_crates):
-            crate = crates[start_crate-1].pop()
-            crates[target_crate-1].append(crate)
-            continue
-
-    return getTopCratesStr(crates)
-
 def reverseCrates(crates):
     for crate in crates:
         crate = crate.reverse()
@@ -61,16 +52,45 @@ def getTopCratesStr(crates):
         out += crate[-1]
     return out
 
+def getPartOneSolution(crates, moves):
+    reverseCrates(crates)
+    for n_crates, start_crate, target_crate in moves:
+        for i in range(n_crates):
+            crate = crates[start_crate-1].pop()
+            crates[target_crate-1].append(crate)
+
+    return getTopCratesStr(crates)
+
+def getPartTwoSolution(crates, moves):
+    reverseCrates(crates)
+    for n_crates, start_crate, target_crate in moves:
+        n_moving_crates = n_crates*-1
+        n_staying_crates = len(crates[start_crate-1]) - abs(n_moving_crates)
+        
+        moving_crates = crates[start_crate-1][n_moving_crates:]
+        crates[start_crate-1] = crates[start_crate-1][:n_staying_crates]
+        crates[target_crate-1].extend(moving_crates)
+
+    return getTopCratesStr(crates)
+
 def main():
     """ Tests """
     crates, moves = getData('data/day-5-test.txt')
-    res = getDayOneSolution(crates[:], moves)
+    res = getPartOneSolution(crates, moves)
     assert(res == 'CMZ')
+    crates, moves = getData('data/day-5-test.txt')
+    res = getPartTwoSolution(crates, moves)
+    assert(res == 'MCD')
 
     """ Solutions """
     crates, moves = getData('data/day-5.txt')
-    res = getDayOneSolution(crates[:], moves)
+    res = getPartOneSolution(crates[:], moves)
     print('Part one solution: {}'.format(res))
+    assert(res == 'TQRFCBSJJ')
+
+    crates, moves = getData('data/day-5.txt')
+    res = getPartTwoSolution(crates[:], moves)
+    print('Part two solution: {}'.format(res))
 
     return 0
 

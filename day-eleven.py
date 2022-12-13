e4dc99d29bcc1d4be0e749864397d203fc2f5778
celsuss <jenslorden@gmail.com>
2022-12-13 02:11:13 +0100

    Started on day eleven

---
+++ day-eleven.py
@@ -0,0 +1,40 @@
+
+class Monkey:
+    def __init__(self, items=None, operation=None, test=None, \
+                    if_true_target=None, if_false_target=None) -> None:
+        _items = items
+        _operation = operation
+        _test = test
+        _if_true_target = if_true_target
+        _if_false_target = if_false_target
+        pass
+
+def getPartOneSolution():
+
+    return 0
+
+def getInput(path):
+    f = open(path)
+    lines = f.readlines()
+    monkeys = []
+    monkey = Monkey
+
+    for i in range(len(lines)):
+        for j in range(6):
+
+            continue
+        monkeys.append(monkey)
+
+    return 0
+
+def main():
+    """ Tests """
+    monkeys = getInput('data/day-11-test.txt')
+
+    """ Solutions """
+
+    return 0
+
+
+if __name__ == '__main__':
+    main()

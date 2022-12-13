f1259529a9f203b464998e245f6a1de8adc3d95b
celsuss <jenslorden@gmail.com>
2022-12-13 01:54:34 +0100

    Day ten part two done

--- day-ten.py
+++ day-ten.py
@@ -1,3 +1,4 @@
+import numpy as np
 
 NOOP = 'noop'
 ADDX = 'addx'
@@ -44,6 +45,52 @@ def getPartOneSolution(instructions):
     signal_strength = getSignalStrength(instructions, x, cycles)
     return signal_strength
 
+def updateScreen(screen, cycle, render_x, x):
+    if abs(render_x - x) >= 2:
+        return
+    screen[cycle] = '#'
+
+def updateRenderPosition(cycle, render_x, width):
+    cycle += 1
+    render_x += 1
+    if render_x >= width:
+        render_x = 0
+    return cycle, render_x
+
+def printScreen(screen):
+    line = ''
+    for row in screen:
+        for c in row:
+            if c == '#':
+                line += '█'
+            else:
+                line += ' '
+        print('{}'.format(line))
+        line = ''
+
+def getPartTwoSolution(instructions):
+    width, height = 40, 6
+    screen = ['.'] * (height * width)
+    x = 1
+    cycle = 0
+    render_x = 0
+
+    for instruction in instructions:
+        inst, val = parseInstruction(instruction)
+        if inst is NOOP:
+            updateScreen(screen, cycle, render_x, x)
+            cycle, render_x = updateRenderPosition(cycle, render_x, width)
+        else:
+            updateScreen(screen, cycle, render_x, x)
+            cycle, render_x = updateRenderPosition(cycle, render_x, width)
+
+            updateScreen(screen, cycle, render_x, x)
+            x += val
+            cycle, render_x = updateRenderPosition(cycle, render_x, width)
+
+    return np.array(screen).reshape((height, width))
+    
+
 def getInput(path):
     f = open(path)
     lines = f.readlines()
@@ -54,12 +101,16 @@ def main():
     instructions = getInput('data/day-10-test.txt')
     res = getPartOneSolution(instructions)
     assert res == 13140, 'Part one test is incorrect'
+    res = getPartTwoSolution(instructions)
 
     """ Solution """
     instructions = getInput('data/day-10.txt')
     res = getPartOneSolution(instructions)
     print('Part one solution: {}'.format(res))
-    assert res > 14340, 'Part one solution is incorrect'
+    assert res == 14340, 'Part one solution is incorrect'
+    res = getPartTwoSolution(instructions)
+    print('Part two solution:')
+    printScreen(res)
 
     return 0
 

8291f57ad7b894a9ef2686f8172c9bf0ce827b0e
Jens <jenslorden@gmail.com>
2023-01-29 13:32:26 +0100

    Solved day twelve part one

--- day-twelve.py
+++ day-twelve.py
@@ -6,9 +6,9 @@ class Node:
     def __init__(self, pos: posTuple, height: int) -> None:
         self.pos = pos
         self.height = height
-        self.G = None
-        self.H = None
-        self.F = None
+        self.g = 0
+        self.h = 0
+        self.f = 0
 
 nodeList = list[Node]
 
@@ -26,42 +26,42 @@ def getG(parent_node: Node) -> int:
 
 def getH(node_pos: posTuple, target_pos: posTuple) -> int:
     """ h = estimate cost from node to target node (manhattan distance) """
-    return (target_pos[0] - node_pos[0]) + (target_pos[1] - node_pos[1])
+    return abs(node_pos[0] - target_pos[0]) + abs(node_pos[1] - target_pos[1])
 
 def getF(node: Node) -> int:
     """ f = sum(g, h)"""
-    return node.G + node.H
+    return node.g + node.h
 
 def addNeighbors(pos: posTuple, height: int, grid: chrGrid) -> nodeList:
     """ Return node with lowest F Cost """
     neighbors = []
-    top_n_pos = (pos[1]-1, pos[0])
-    right_n_pos = (pos[1], pos[0]+1)
-    bot_n_pos = (pos[1]+1, pos[0])
-    left_n_pos = (pos[1], pos[0]-1)
+    top_n_pos = (pos[0], pos[1]-1)
+    right_n_pos = (pos[0]+1, pos[1])
+    bot_n_pos = (pos[0], pos[1]+1)
+    left_n_pos = (pos[0]-1, pos[1])
 
-    if top_n_pos[0] > 0 and \
+    if top_n_pos[1] >= 0 and \
         isAccessible(height, getHeight(top_n_pos, grid)):
         neighbors.append(Node(top_n_pos, getHeight(top_n_pos, grid)))
 
-    if right_n_pos[1] < len(grid[0])-1 and \
+    if right_n_pos[0] <= len(grid[0])-1 and \
         isAccessible(height, getHeight(right_n_pos, grid)):
         neighbors.append(Node(right_n_pos, getHeight(right_n_pos, grid)))
 
-    if bot_n_pos[0] < len(grid)-1 and \
+    if bot_n_pos[1] <= len(grid)-1 and \
         isAccessible(height, getHeight(bot_n_pos, grid)):
         neighbors.append(Node(bot_n_pos, getHeight(bot_n_pos, grid)))
 
-    if left_n_pos[1] > 0 and \
+    if left_n_pos[0] >= 0 and \
         isAccessible(height, getHeight(left_n_pos, grid)):
         neighbors.append(Node(left_n_pos, getHeight(left_n_pos, grid)))
 
     return neighbors
 
-def getBetterNode(nodes: nodeList, node: Node):
+def getNodeWithLowerFScore(nodes: nodeList, node: Node):
     for n in nodes:
         if n.pos == node.pos:
-            if n.F < node.F:
+            if n.f <= node.f:
                 return n
             else:
                 break
@@ -71,9 +71,9 @@ def getLowestFScoreNode(nodes: nodeList):
     """ Return node with lowest F Score """
     if len(nodes) == 0:
         return None
-    res = nodes[0].F
+    res = nodes[0]
     for node in nodes[1:]:
-        if node.F < res.F:
+        if node.f < res.f:
             res = node
     return res
 
@@ -99,17 +99,16 @@ def aStar(grid: chrGrid, start: posTuple, target: posTuple):
         neighbors = addNeighbors(node.pos, node.height, grid)
         for neighbor in neighbors:
             if neighbor.pos == target:
-                break
-            neighbor.G = getG(node)
-            neighbor.H = getH(target)
-            neighbor.F = getF(neighbor)
-            if getBetterNode(open_list, node) is not None:
-                continue
-            open_list.append(neighbor)
-            continue
+                return node.g + 1
+
+            neighbor.g = getG(node)
+            neighbor.h = getH(node.pos, target)
+            neighbor.f = getF(neighbor)
+            if getNodeWithLowerFScore(open_list, neighbor) is None and \
+                getNodeWithLowerFScore(closed_list, neighbor) is None:
+                open_list.append(neighbor)
 
         closed_list.append(node)
-        continue
 
     return 0
 
@@ -145,9 +144,9 @@ def getStartAndTarget(lines):
         for j in range(len(line)):
             c = line[j]
             if c == 'S':
-                start = (i, j)
+                start = (j, i)
             elif c == 'E':
-                target = (i, j)
+                target = (j, i)
 
     return start, target
 
@@ -170,9 +169,11 @@ def main():
                         is {} but should be 31'.format(res)
 
     """ Solutions """
-    grid = getInput('data/day-12.txt')
-    res = solvePartOne(grid)
+    grid, start, target = getInput('data/day-12.txt')
+    res = solvePartOne(grid, start, target)
     print('Part one solution: {}'.format(res))
+    assert res == 504, 'Part one is incorrect, \
+                        is {} but should be 504'.format(res)
 
     return 0
 
